758093a5ee8ab33fd0b3e8c685fe3812fbdacec8
ravindurasanjana <ravindu.2019802@iit.ac.lk>
2021-06-13 02:26:18 +0530

    history page ui change

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import Login from './components/Login';
-import {Container, Root} from 'native-base';
+import { Container, Root } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState } from "react";
 import { createStackNavigator } from '@react-navigation/stack';
+import History from './components/History';
 
 export default function App() {
   const [profile, setProfile] = useState(null);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const signout = () => {
-      setIsUserLoggedIn(false);
-      setProfile(null);
+    setIsUserLoggedIn(false);
+    setProfile(null);
   };
 
   const login = async (user) => {
@@ -20,36 +21,27 @@ export default function App() {
       userId: "sadeshS",
       password: "123456789"
     }
-    if(user.userId == existingUser.userId && user.password == existingUser.password){
+    if (user.userId == existingUser.userId && user.password == existingUser.password) {
       setProfile(existingUser);
       setIsUserLoggedIn(true);
     }
   }
 
   const Stack = createStackNavigator();
-  
+
   return (
     <Root>
-      <Container style={styles.container}>
-        {!isUserLoggedIn ? (
-          <Login login={login}/>
-        ):(
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName={"Home"}>
-              <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        )}
-      </Container>
+      {/* <View> */}
+      {!isUserLoggedIn ? (
+        <Login login={login} />
+      ) : (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={"Home"}>
+            <Stack.Screen name="Home" component={History} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+      {/* </View> */}
     </Root>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
